aca16c7427ef1526511fdc442817b92c1211ce9b
Chandon Porter <cneilporter94@gmail.com>
2019-08-08 16:55:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,43 +1,42 @@
-class Book
+class Shoe
   
-  def initialize(title)
-    @title = title
-end
-
-def title
-  @title
-end
-
-def author=(author)
-  @author = author
-end
-
-def author
-  @author
-end
-
-def page_count=(num)
-  @page_count = num
-end
-
-def page_count
-  @page_count
-end
-
-def genre=(genre)
-  @genre = genre
-end
-
-def genre
-  @genre
-end
-
-def turn_page
-  puts "Flipping the page...wow, you read fast!"
-end
-
-
-
-end
-
-Book.new("And Then There Were None")
+    def initialize(color)
+      @color = color
+  end
+  
+  def color
+    @color
+  end
+  
+  def size=(size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition "new"
+  end
+  
+  end
+  
+  Shoe.new("Nike")
